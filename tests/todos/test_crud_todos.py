ce68287e14b64646c21b65435854e63dce433503
UlrichIvan <mtchokos@gmail.com>
2025-06-27 21:24:06 +0200

    test add todo with authentication done successfully

--- tests/todos/test_crud_todos.py
+++ tests/todos/test_crud_todos.py
@@ -49,3 +49,70 @@ class TodosListViewTest(TestCase):
         res = self.client.get(path=reverse("todo_list:index"))
         self.assertEqual(res.status_code, 302)
         self.assertRedirects(res, reverse("todo_list:todo_user_login"))
+
+
+class AddTodoViewTest(TestCase):
+
+    def setUp(self) -> None:
+        self.client = Client()
+        self.code = "97D08BB6"
+        self.user_form = {
+            "first_name": "test1",
+            "last_name": "test2",
+            "email": "ab@gmail.com",
+            "actived": False,
+            "code": self.code,
+        }
+        self.password = "testazertyaa"
+        self.create_user()
+
+    def create_user(self):
+        self.user = UserTodo(
+            **self.user_form,
+            password=bcrypt.hashpw(self.password.encode(), bcrypt.gensalt()).decode(),
+        )
+        self.user.save()
+        self.token = get_jwt_token(
+            payload={
+                "is_auth": True,
+                "user_id": str(self.user.id),
+                "user_name": self.user.last_name,
+                "photo": None,
+                "iat": datetime.datetime.now(),
+                "exp": datetime.datetime.now() + datetime.timedelta(days=365),
+            }
+        )
+        session = self.client.session
+        session["token"] = self.token
+        session.save()
+
+    def test_add_todo_get(self):
+        res = self.client.get(path=reverse("todo_list:add_todo"))
+        self.assertEqual(res.status_code, 200)
+
+    def test_add_todo_pass(self):
+        res = self.client.post(
+            path=reverse("todo_list:add_todo"),
+            data={
+                "title": "Test Todo",
+                "content": "This is a test todo item.",
+                "done": False,
+            },
+            follow=True,
+        )
+        self.assertEqual(res.status_code, 200)
+        messages = list(res.context["messages"])
+        self.assertTrue(any("todo has been created successfully. create another todo if you want" in str(m) for m in messages))
+    def test_add_todo_fail(self):
+        res = self.client.post(
+            path=reverse("todo_list:add_todo"),
+            data={
+                "title": "",
+                "content": "This is a test todo item.",
+                "done": False,
+            },
+            follow=True,
+        )
+        self.assertEqual(res.status_code, 200)
+        messages = list(res.context["messages"])
+        self.assertTrue(any("invalid form data" in str(m) for m in messages))
